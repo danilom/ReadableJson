6f782fc3cc8e085f15a250d2ce41f6759a3b4e6a
Danilo <danilom@gmail.com>
2021-11-04 22:57:43 +0100

    Indentation support

--- ts/src/ReadabjeJson.ts
+++ ts/src/ReadabjeJson.ts
@@ -2,45 +2,81 @@
 type ReplacerT = (this: any, key: string, value: any) => any | (number | string)[] | null;
 
 export default class readabjeJson {
-
   static stringify(value: any, replacer?: ReplacerT, space?: string | number): string {
-    return JSON.stringify(value, replacer, space);
+    space = spaceStr(space);
+    const opt: IOptions = {
+      space: space,
+      newline: space.length == 0 ? "" : "\n"
+    }
+    return stringify(value, 0, opt);
   }
+}
+
+function spaceStr(space?: string | number) : string {
+  if (typeof(space) == "number") {
+    space = " ".repeat(space);
+  }
+  else if (typeof(space) == "string") {
+    space = space;
+  }
+  else {
+    // Default is 2 spaces, unlike 0 in JSON.stringify
+    space = "  ";
+  }
+  if (space.length > 10) { space.substr(0, 10); }
+  return space;
+}
+
+interface IOptions {
+  space: string;
+  newline: string;
 }
 
 // TODO: circular references
 
 // Based on:
 // https://stackoverflow.com/a/43652073/838
-function stringifyJSON(data: any): string | undefined {
-  if (data === undefined)
-    return undefined
-  else if (data === null)
-    return 'null'
-  /*
-  else if (data.constructor === String)
-    return '"' + data.replace(/"/g, '\\"') + '"'
-  else if (data.constructor === Number)
-    return String(data)
-  else if (data.constructor === Boolean)
-    return data ? 'true' : 'false'
-  */
-  else if (data.constructor === Array)
-    return '[ ' + data.reduce((acc, v) => {
-      if (v === undefined)
-        return [...acc, 'null']
-      else
-        return [...acc, stringifyJSON(v)]
-    }, []).join(', ') + ' ]'
-  else if (data.constructor === Object)
-    return '{ ' + Object.keys(data).reduce((acc: any, k: any) => {
-      if (data[k] === undefined)
-        return acc
-      else
-        return [...acc, stringifyJSON(k) + ':' + stringifyJSON(data[k])]
-    }, []).join(', ') + ' }'
-  else
-    return JSON.stringify(data);
-    //return '{}'
+function stringify(data: any, level: number, opt: IOptions): string {
+  if (data === undefined) {
+    return <any>undefined;
+  }
+  if (data === null) {
+    return 'null';
+  }
+  if (Array.isArray(data)) {
+    return stringifyArray(data, level, opt);
+  }
+  if (typeof(data) === "object") {
+    return stringifyObject(data, level, opt);
+  }
+  return JSON.stringify(data);
 }
 
+function stringifyArray(data: any, level: number, opt: IOptions) : string {
+  const indent = opt.space.repeat(level);
+  return '[' + opt.newline 
+    + indent + opt.space
+    + data.reduce((acc: any, v: any) => {
+        if (v === undefined)
+          return [...acc, 'null']
+        else
+          return [...acc, stringify(v, level + 1, opt)]
+      }, [])
+    .join(',' + opt.newline + indent + opt.space) + opt.newline
+    + indent + ']';
+}
+
+function stringifyObject(data: any, level: number, opt: IOptions) {
+  const indent = opt.space.repeat(level);
+  return '{' + opt.newline 
+    + indent + opt.space
+    + Object.keys(data).reduce((acc: any, k: any) => {
+        if (data[k] === undefined)
+          return acc;
+        else
+          return [...acc, stringify(k, level + 1, opt) + ': '
+                        + stringify(data[k], level + 1, opt)];
+      }, [])
+    .join(',' + opt.newline + indent + opt.space) + opt.newline 
+    + indent + '}';
+}
--- ts/tests/ReadableJson.test.ts
+++ ts/tests/ReadableJson.test.ts
@@ -1,5 +1,6 @@
 import RJSON from '../src/ReadabjeJson';
 
+/*
 describe('stringify', () => {
   it('null', () => sameAsBuiltIn(null));
   it('number', () => sameAsBuiltIn(123));
@@ -7,30 +8,36 @@ describe('stringify', () => {
   it('object-empty', () => sameAsBuiltIn({}));
   it('array-empty', () => sameAsBuiltIn([]));
 });
+*/
 
 // https://stackoverflow.com/a/43652073/838
 describe('stack-overflow', () => {
   test(null)                               // null
   test('he said "hello"')                  // 'he said "hello"'
-  test(5)                                  // 5
-  test([1,2,true,false])                   // [ 1, 2, true, false ]
+  test(5)                                  // 5  
+  test([1,2,true,false])                   // [ 1, 2, true, false ]  
   test({a:1, b:2})                         // { a: 1, b: 2 }
   test([{a:1},{b:2},{c:3}])                // [ { a: 1 }, { b: 2 }, { c: 3 } ]
   test({a:[1,2,3], c:[4,5,6]})             // { a: [ 1, 2, 3 ], c: [ 4, 5, 6 ] }
   test({a:undefined, b:2})                 // { b: 2 }
   test({[<any>undefined]: 1})                   // { undefined: 1 }
   test([[["test","mike",4,["jake"]],3,4]]) // [ [ [ 'test', 'mike', 4, [ 'jake' ]
+
+  // Multiple levels
+  test([0, [1, [2, [3]]]]);
+  test([0, {a: 0, b: [1, {a: 1, b:[2, null] }]}]);  
 });
 
-function test(value: any) {
-  it(JSON.stringify(value), () => sameAsBuiltIn(value));
-}
-
-function sameAsBuiltIn(value: any) {
+function test(value: any, shape?: string) {
+  const testName = JSON.stringify(value).substr(0, 20);
   const readableJson = RJSON.stringify(value);
   const newValue = JSON.parse(readableJson);
 
-  console.log(readableJson);
+  it(`equal ${testName}`, () => expect(newValue).toEqual(value));
 
-  expect(newValue).toEqual(value);
+  // TODO: remove
+  // If shape isn't specified, use the default
+  shape = shape || JSON.stringify(value, null, 2);
+
+  it(`shape ${testName}`, () => expect(readableJson).toEqual(shape) );
 }
