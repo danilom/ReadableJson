d4178abb9e7f8d3113c90a0390d66fa7b7469796
Danilo <danilom@gmail.com>
2021-11-04 21:31:39 +0100

    Basic implementation from StackOverflow

--- ts/src/ReadabjeJson.ts
+++ ts/src/ReadabjeJson.ts
@@ -6,5 +6,41 @@ export default class readabjeJson {
   static stringify(value: any, replacer?: ReplacerT, space?: string | number): string {
     return JSON.stringify(value, replacer, space);
   }
-
 }
+
+// TODO: circular references
+
+// Based on:
+// https://stackoverflow.com/a/43652073/838
+function stringifyJSON(data: any): string | undefined {
+  if (data === undefined)
+    return undefined
+  else if (data === null)
+    return 'null'
+  /*
+  else if (data.constructor === String)
+    return '"' + data.replace(/"/g, '\\"') + '"'
+  else if (data.constructor === Number)
+    return String(data)
+  else if (data.constructor === Boolean)
+    return data ? 'true' : 'false'
+  */
+  else if (data.constructor === Array)
+    return '[ ' + data.reduce((acc, v) => {
+      if (v === undefined)
+        return [...acc, 'null']
+      else
+        return [...acc, stringifyJSON(v)]
+    }, []).join(', ') + ' ]'
+  else if (data.constructor === Object)
+    return '{ ' + Object.keys(data).reduce((acc: any, k: any) => {
+      if (data[k] === undefined)
+        return acc
+      else
+        return [...acc, stringifyJSON(k) + ':' + stringifyJSON(data[k])]
+    }, []).join(', ') + ' }'
+  else
+    return JSON.stringify(data);
+    //return '{}'
+}
+
--- ts/tests/ReadableJson.test.ts
+++ ts/tests/ReadableJson.test.ts
@@ -1,6 +1,6 @@
 import RJSON from '../src/ReadabjeJson';
 
-describe('stringify', function() {
+describe('stringify', () => {
   it('null', () => sameAsBuiltIn(null));
   it('number', () => sameAsBuiltIn(123));
   it('string', () => sameAsBuiltIn("abc '123' and \"456\""));
@@ -8,9 +8,29 @@ describe('stringify', function() {
   it('array-empty', () => sameAsBuiltIn([]));
 });
 
+// https://stackoverflow.com/a/43652073/838
+describe('stack-overflow', () => {
+  test(null)                               // null
+  test('he said "hello"')                  // 'he said "hello"'
+  test(5)                                  // 5
+  test([1,2,true,false])                   // [ 1, 2, true, false ]
+  test({a:1, b:2})                         // { a: 1, b: 2 }
+  test([{a:1},{b:2},{c:3}])                // [ { a: 1 }, { b: 2 }, { c: 3 } ]
+  test({a:[1,2,3], c:[4,5,6]})             // { a: [ 1, 2, 3 ], c: [ 4, 5, 6 ] }
+  test({a:undefined, b:2})                 // { b: 2 }
+  test({[<any>undefined]: 1})                   // { undefined: 1 }
+  test([[["test","mike",4,["jake"]],3,4]]) // [ [ [ 'test', 'mike', 4, [ 'jake' ]
+});
+
+function test(value: any) {
+  it(JSON.stringify(value), () => sameAsBuiltIn(value));
+}
+
 function sameAsBuiltIn(value: any) {
   const readableJson = RJSON.stringify(value);
   const newValue = JSON.parse(readableJson);
+
+  console.log(readableJson);
 
   expect(newValue).toEqual(value);
 }
