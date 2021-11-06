6511b51395e34e1a6b3a1f3b5bb5a225b13536f9
Danilo <danilom@gmail.com>
2021-11-06 17:49:28 +0100

    Inlining and key alignment (tested for correctness, not shape in advanced cases)

--- ts/src/ReadabjeJson.ts
+++ ts/src/ReadabjeJson.ts
@@ -1,73 +1,232 @@
 
 type ReplacerT = (this: any, key: string, value: any) => any | (number | string)[] | null;
 
+interface IOptions {
+  spaceLen: number;
+  newline: string;
+
+  maxKeyAlignLen: number;
+  maxInlineDepth: number;
+  maxInlineLen: number;
+}
+
 export default class readabjeJson {
-  static stringify(value: any, replacer?: ReplacerT, space?: string | number): string {
-    space = spaceStr(space);
-    const opt: IOptions = {
-      space: space,
-      newline: space.length == 0 ? "" : "\n",
+  // TODO: replacer
+  // TODO-??: space is string|number in the original
+  static stringify(value: any, replacer?: ReplacerT, space?: number): string {
+    // With no spaces, built-in stringifier is faster
+    if (space == 0) {
+      return JSON.stringify(value, replacer, space);
     }
+
+    const opt: IOptions = {
+      spaceLen: space || 2,
+      newline: "\n",
+      maxKeyAlignLen: 16,
+      maxInlineDepth: 2,
+      maxInlineLen: 100,
+    }
+    //return stringifyV1(value, 0, opt);
     return stringify(value, 0, opt);
   }
+
+  // test
+  static test_canInlineByLeafDepth = canInlineByLeafDepth;
 }
 
-function spaceStr(space?: string | number) : string {
-  if (typeof(space) == "number") {
-    space = " ".repeat(space);
-  }
-  else if (typeof(space) == "string") {
-    space = space;
-  }
-  else {
-    // Default is 2 spaces, unlike 0 in JSON.stringify
-    space = "  ";
-  }
-  if (space.length > 10) { space.substr(0, 10); }
-  return space;
+interface KVItem {
+  key: any;
+  keyJson: string;
+  v: any;
+  //vJson?: string;
 }
 
-interface IOptions {
-  space: string;
-  newline: string;
-}
-
-// TODO: circular references
-
-// Based on:
-// https://stackoverflow.com/a/43652073/838
-function stringify(data: any, level: number, opt: IOptions): string {
-  if (data !== null && typeof(data) === "object") {
-    return stringifyObject(data, level, opt);
+function stringify(data: any, indentLen: number, opt: IOptions): string {
+  if (data === null || typeof(data) !== "object") {
+    // Primitive
+    return JSON.stringify(data);
   }
-  // Primitives
-  return JSON.stringify(data);
+
+  // Object or array
+  
+  // Try to stringify inline
+  let inlineJson: string | null = null;
+  if (canInlineByLeafDepth(data, opt.maxInlineDepth)) {
+    inlineJson = tryStringifyInline(data, opt.maxInlineLen - indentLen);
+  }
+  if (inlineJson != null) {
+    return inlineJson;
+  }
+  
+  // Couldn't inline, use the full-length method
+  return stringifyFull(data, indentLen, opt)
 }
 
-
-function stringifyObject(data: any, level: number, opt: IOptions) {
-  const indent = opt.space.repeat(level);
-  const items: string[] = [];
-  let [open, close] = ['{', '}']
-
+function stringifyFull(data: any, indentLen: number, opt: IOptions): string {
+  if (data === null || typeof(data) !== "object") {
+    // Primitive, always inline
+    return JSON.stringify(data);
+  }
   if (Array.isArray(data)) {
-    [open, close] = ['[', ']'];
-    for(const v of data) {
-      if (v === undefined) { items.push('null'); }
-      else { items.push(stringify(v, level + 1, opt)); }
+    // Array, similar to objects
+    return stringifyFullArray(data, indentLen, opt);
+  }
+
+  // Object: {"a": 1, b: "2"}
+
+  // Key alignment
+  const keyIndentLen = indentLen + opt.spaceLen;
+  const keyIndent = " ".repeat(keyIndentLen);
+  const kvInfo = getSortedKVs(data, opt);
+
+  let str = "{";
+  let first = true;
+  for(var kvi of kvInfo.sortedKVs) {
+    if (first) { first = false; }
+    else { str += ","; }
+
+    str += "\n" + keyIndent;
+    
+    // Insert spaces for key alignment
+    let keyJson = kvi.keyJson;
+    if (keyJson.length < kvInfo.keyAlignLen) {
+      const pad = " ".repeat(kvInfo.keyAlignLen - keyJson.length);
+      if (typeof(kvi.key) === 'number') { keyJson = pad + keyJson; } // right-align numbers
+      else { keyJson += pad; }
+    }
+    str += keyJson + ': ';
+    str += stringify(kvi.v, (keyIndentLen + keyJson.length + 2), opt);
+  }
+  str += "\n" + " ".repeat(indentLen) + "}";
+
+  return str;
+}
+function stringifyFullArray(data: any, indentLen: number, opt: IOptions): string {
+  const vIndentLen = indentLen + opt.spaceLen;
+  const vIndent = " ".repeat(vIndentLen);
+
+  let str = "[";
+  let first = true;
+  for(var v of data) {
+    if (first) { first = false; }
+    else { str += ","; }
+
+    str += opt.newline + vIndent;
+    str += stringify(v, vIndentLen, opt);
+  }
+  str += opt.newline + " ".repeat(indentLen) + "]";
+
+  return str;
+}
+
+function tryStringifyInline(data: any, maxInlineLen: number): string | null {
+  if (data === null || typeof(data) !== "object") {
+    // Primitive, always inline
+    return JSON.stringify(data);
+  }
+  if (Array.isArray(data)) {
+    // Array, similar to objects
+    return tryStringifyInlineArray(data, maxInlineLen);
+  }
+
+  // Object
+  // {"a":1, "b":2}
+  let str = '{'; 
+  let first = true;
+  for(const key of Object.keys(data)) {
+    const v = data[key];
+    if (v === undefined) { continue; }
+
+    if (first) { first = false; }
+    else { str += ", "; } // Extra space for compactness. TODO: make configurable
+
+    if (str.length > maxInlineLen) { return null; }
+
+    const keyJson = tryStringifyInline(key, maxInlineLen - str.length);
+    if (keyJson === null) { return null; }
+    str += keyJson + ":"; // Single space for compactness
+
+    if (str.length > maxInlineLen) { return null; }
+
+    const vJson = tryStringifyInline(v, maxInlineLen - str.length);
+    if (vJson === null) { return null; }
+    str += vJson;
+  }
+  str += "}";
+  if (str.length > maxInlineLen) { return null; }
+  return str;
+}
+function tryStringifyInlineArray(data: any, maxInlineLen: number): string | null {
+  // [1, 2, 3]
+  let str = '['; 
+  let first = true;
+  for(const v of data) {
+    if (first) { first = false; }
+    else { str += ", "; } // Extra space for compactness. TODO: make configurable
+
+    if (str.length > maxInlineLen) { return null; }
+
+    if (v === undefined) { str += 'null'; }
+    else {
+      const vJson = tryStringifyInline(v, maxInlineLen - str.length);
+      if (vJson === null) { return null; }
+      str += vJson;
     }
   }
-  else {
-    for(const key of Object.keys(data)) {
-      const value = data[key]; 
-      if (value !== undefined) { 
-        items.push(stringify(key, level + 1, opt) + ': ' + stringify(value, level + 1, opt));
+  str += "]";
+  if (str.length > maxInlineLen) { return null; }
+  return str;
+}
+
+function canInlineByLeafDepth(data: any, maxDepth: number): boolean {
+  if (maxDepth < 0) { return false; }
+
+  if (data !== null && typeof(data) === "object") {
+    // Arrays/objects: compute depth for each child, add 1
+    if (Array.isArray(data)) 
+    {
+      for(let item of data) {
+        if (!canInlineByLeafDepth(item, maxDepth - 1)) { return false; }
       }
     }
+    else 
+    {
+      for (let key of Object.keys(data)) {
+        if (!canInlineByLeafDepth(data[key], maxDepth - 1)) { return false; }
+      }
+    }
+    // Can inline all items, OK
+    return true;
+  }
+  // Primitives: can always inline
+  return true;
+}
+
+
+function getSortedKVs(data: any, opt: IOptions) {
+  const rv = {
+    // Common width to align keys.
+    // Maximum of key JSON representations, excluding those over opt.maxKeyAlignLen
+    keyAlignLen: 0,
+    sortedKVs: <KVItem[]>[]
+  };
+
+  // TODO: have customizable sorting
+  const sortedKeys = Object.keys(data).sort();
+
+  for(const key of sortedKeys) {
+    const v = data[key];
+    if (v === undefined) { continue; } // skip undefined items
+    
+    var keyJson = JSON.stringify(key);
+    var kvItem: KVItem = { key: key, keyJson: keyJson, v: v };
+    if (keyJson.length <= opt.maxKeyAlignLen
+        && keyJson.length > rv.keyAlignLen) 
+    {
+      rv.keyAlignLen = keyJson.length;
+    }
+    rv.sortedKVs.push(kvItem);
   }
 
-  return open + opt.newline
-              + indent + opt.space
-              + items.join(',' + opt.newline + indent + opt.space) + opt.newline
-              + indent + close;
+  return rv;
 }
--- ts/tests/ReadableJson.test.ts
+++ ts/tests/ReadableJson.test.ts
@@ -1,29 +1,53 @@
 import RJSON from '../src/ReadabjeJson';
 
-/*
-describe('stringify', () => {
-  it('null', () => sameAsBuiltIn(null));
-  it('number', () => sameAsBuiltIn(123));
-  it('string', () => sameAsBuiltIn("abc '123' and \"456\""));
-  it('object-empty', () => sameAsBuiltIn({}));
-  it('array-empty', () => sameAsBuiltIn([]));
-});
-*/
+// describe('stringify', () => {
+//   it('null', () => sameAsBuiltIn(null));
+//   it('number', () => sameAsBuiltIn(123));
+//   it('string', () => sameAsBuiltIn("abc '123' and \"456\""));
+//   it('object-empty', () => sameAsBuiltIn({}));
+//   it('array-empty', () => sameAsBuiltIn([]));
+// });
 
 // https://stackoverflow.com/a/43652073/838
-describe('stack-overflow', () => {
-  test('null', null)                              // null
-  test('string', 'he said "hello"')               // 'he said "hello"'
-  test('number', 5)                               // 5  
-  test('array', [1,2,true,false])                 // [ 1, 2, true, false ]  
-  test('object', {a:1, b:2})                      // { a: 1, b: 2 }
-  test('obj in array', [{a:1},{b:2},{c:3}])       // [ { a: 1 }, { b: 2 }, { c: 3 } ]
-  test('array in obj', {a:[1,2,3], c:[4,5,6]})    // { a: [ 1, 2, 3 ], c: [ 4, 5, 6 ] }
-  test('undefined as value', {a:undefined, b:2})  // { b: 2 }
-  test('undefined as key', {undefined: 1})        // { undefined: 1 }
-  test('undefined as key', {undefined: 1})        // { undefined: 1 }
-  test('null as key', {null: 1})                  // { null: 1 }
-  test('mixed', [[["test","mike",4,["jake"]],3,4]]) // [ [ [ 'test', 'mike', 4, [ 'jake' ]
+describe('stack-overflow cases', () => {
+  test('null', null)
+  test('string', 'he said "hello"')
+  test('number', 5)
+
+  test('array',
+    [1,2,true,false],
+    `[1, 2, true, false]`) // Inline
+  test('object', 
+    {a:1, b:2},
+    `{"a":1, "b":2}`) // Inline
+  test('obj in array', 
+    [{a:1},{b:2},{c:3}],
+    `[{"a":1}, {"b":2}, {"c":3}]`) // Inline
+
+  test('array in obj', 
+    {a:[1,2,3], c:[4,5,6]},
+    `{"a":[1, 2, 3], "c":[4, 5, 6]}`) // Inline
+    
+  test('undefined as value', 
+    {a:undefined, b:2},
+    `{"b":2}`); // Inline, eliminated extra key
+
+  test('undefined as key', 
+    {undefined: 1},
+    '{"undefined":1}')
+  test('null as key', 
+    {null: 1},
+    `{"null":1}`)
+  test('mixed', 
+    [[["test","mike",4,["jake"]],3,4]],
+`[
+  [
+    ["test", "mike", 4, ["jake"]],
+    3,
+    4
+  ]
+]`)
+    
 
   // edge case, round-tripping doesn't work
   test('undefined (null) in arr', [1, undefined], "", false)    // [ 1, null]; converts to "null"
@@ -32,6 +56,25 @@ describe('stack-overflow', () => {
   test('arr levels', [0, [1, [2, [3]]]]);
   test('obj levels', [0, {a: 0, b: [1, {a: 1, b:[2, null] }]}]);  
 });
+
+// describe('canInlineByLeafDepth', () => {
+//   const ci = RJSON.test_canInlineByLeafDepth;
+//   const maxDepth = 2;
+
+//   it('primitive', () => expect(ci("foo", maxDepth)).toBeTruthy());
+//   it('array', () => expect(ci([0,1,2], maxDepth)).toBeTruthy());
+
+//   it('array 1 deep', () => expect(ci([0,[1,2]], maxDepth)).toBeTruthy());
+//   it('array 2 deep', () => expect(ci([0,[1,[2,3]]], maxDepth)).toBeFalsy());
+//   it('array 3 deep', () => expect(ci([0,[1,[2,[3,4]]]], maxDepth)).toBeFalsy());
+
+//   it('obj 1 deep', () => expect(ci({a: 1, b: 2}, maxDepth)).toBeTruthy());
+//   it('obj 2 deep', () => expect(ci({a: 1, b: { c: 3, d: 4}}, maxDepth)).toBeTruthy());
+//   it('obj 3 deep', () => expect(ci({a: 1, b: { c: 3, d: { e: 1, f: 2}}}, maxDepth)).toBeFalsy());
+
+//   it('mixed 2 deep', () => expect(ci({arr: [1, 2], obj: { a: 1, b:2 }}, maxDepth)).toBeTruthy());
+//   it('mixed 3 deep', () => expect(ci({arr: [1, { a: 1 }], obj: { a: 1, b:2 }}, maxDepth)).toBeFalsy());
+// });
 
 // describe('perf', () => {
 //   const arr: number[] = [];
@@ -44,17 +87,31 @@ describe('stack-overflow', () => {
 //   it(`loop`, () => expect(RJSON.stringify(arr, undefined, undefined)).not.toEqual(""));
 // });
 
-function test(testName: string, value: any, shape?: string, isEqual = true) {
+function test(testName: string, value: any, 
+  shape?: string, doEqual = true) 
+{
   const readableJson = RJSON.stringify(value);
-  const newValue = JSON.parse(readableJson);
+  let newValue = "";
+  try 
+  {
+    newValue = JSON.parse(readableJson);
+  } 
+  catch (SyntaxError) {
+    // debug here
+    RJSON.stringify(value);
+    fail(readableJson);
+    return;
+  }
 
-  if (isEqual) {
+  if (doEqual) {
     it(`${testName}`, () => expect(newValue).toEqual(value));
   }
 
+  //console.log(readableJson);
+
   // TODO: remove
   // If shape isn't specified, use the default
-  shape = shape || JSON.stringify(value, null, 2);
-
-  it(`shape ${testName}`, () => expect(readableJson).toEqual(shape) );
+  if (shape) {
+    it(`shape ${testName}`, () => expect(readableJson).toEqual(shape) );  
+  }
 }
